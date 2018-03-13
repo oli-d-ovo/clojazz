821ebc082b3912d0de76b99b93fc9d140e60f53a
Oli Desborough <oli.desborough@ovoenergy.com>
2018-03-13 15:39:27 +0000

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojazz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojazz/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojazz.core-test
+  (:require [clojure.test :refer :all]
+            [clojazz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
