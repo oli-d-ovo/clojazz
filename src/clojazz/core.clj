9b710e50b929877da629f20e3eca89b9c4e30255
Oli Desborough <oli.desborough@ovoenergy.com>
2018-03-27 16:26:05 +0100

    Added main fn

--- src/clojazz/core.clj
+++ src/clojazz/core.clj
@@ -1,7 +1,18 @@
 (ns clojazz.core
+  (:require [overtone.core :as overtone]
+            [clojazz.domain.player :refer [play-tune]])
   (:gen-class))
+
+
+(defn find-tune
+  [tune-name]
+  (->> (str "clojazz.examples." tune-name "/" tune-name)
+       symbol
+       find-var
+       var-get))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& [tune-name]]
+  (overtone/boot-server)
+  (play-tune (find-tune tune-name)))
